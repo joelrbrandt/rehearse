a6e5fb85a8a4ccc2ed0d312eb7dffb846a5dfbc8
bjoern.hartmann <bjoern.hartmann@babe0970-8b47-0410-8b6d-03bcedc6537e>
2009-06-30 04:50:58 +0000

    first running version of echo, database server running on rehearse apache an mod_python

--- helpmeout-server/trunk/helpmeout.py
+++ helpmeout-server/trunk/helpmeout.py
@@ -1,7 +1,14 @@
 from mod_python import apache, util
+import sys,os
 
 def handler(req):
     req.content_type = "text/plain"
+
+    (modulePath, fileName) = os.path.split(req.filename)
+    (moduleName, ext) = os.path.splitext(fileName)
+    req.write(modulePath+ "\n\n")
+    req.write(moduleName+ "\n\n")
+    req.write("Looking up:"+os.path.join(modulePath, moduleName + ".py\n\n"))
     
     req.write("1 + 1 = " + str(1+1) + "\n\n")
 
