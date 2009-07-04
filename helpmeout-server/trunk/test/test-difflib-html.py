c943d4befe8adf79d4305f28e2a7cc7ccaab338c
bjoern.hartmann <bjoern.hartmann@babe0970-8b47-0410-8b6d-03bcedc6537e>
2009-07-04 03:50:04 +0000

    added test for difflib html table generator

---
+++ helpmeout-server/trunk/test/test-difflib-html.py
@@ -0,0 +1,10 @@
+import difflib
+
+before = ['//a comment\n','int x[]=new int[];\n','//something else\n']
+after = ['//a comment\n','int x[]=new int[5];\n','//something else\n','totally different\n']
+
+d = difflib.HtmlDiff()
+html = d.make_table(before,after,"Before (Broken)","After (Fixed)",context=True,numlines=0)
+f= open("out.html","w")
+f.write(html)
+f.close()
