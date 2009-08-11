c0e686544afeb7f035e9197cd66ea8ceb7711475
dmacdougall <dmacdougall@babe0970-8b47-0410-8b6d-03bcedc6537e>
2009-08-11 23:13:05 +0000

    fixed ANOTHER charIndex/lineNum bug

--- rehearse-processing/trunk/test/src/test/LineOffsetTest.java
+++ rehearse-processing/trunk/test/src/test/LineOffsetTest.java
@@ -25,10 +25,9 @@ public class LineOffsetTest extends TestCase {
 		int l0 = HelpMeOutExceptionTracker.getInstance().getCharIndexFromLine(s1,0);
 		int l1 = HelpMeOutExceptionTracker.getInstance().getCharIndexFromLine(s1,1);
 		int l2 = HelpMeOutExceptionTracker.getInstance().getCharIndexFromLine(s1,2);
-		
-		System.out.println(l1);
+
 		assertTrue(l0==0);
-		assertTrue(l1==5);
+		assertTrue(Integer.toString(l1), l1==5);
 		assertTrue(l2==10);
 
 	}
@@ -58,5 +57,19 @@ public class LineOffsetTest extends TestCase {
 		
 		assertTrue(2==HelpMeOutExceptionTracker.getInstance().getLineFromCharIndex(s1,10));
 		
+	}
+	
+	public void testGetLineToWatchAux() {
+		String s1 = "123\n456\n789";
+		String s2 = "123\n4B6\n789";
+		int absLineNum = 1;
+		int returnedLine = HelpMeOutExceptionTracker.getInstance().getLineToWatchAux(s1, s2, absLineNum);
+		assertTrue(Integer.toString(returnedLine), returnedLine==1);
+		
+		s1 = "\n123\n456\n789";
+		s2 = "\n123\n4B6\n789";
+		absLineNum = 2;
+		returnedLine = HelpMeOutExceptionTracker.getInstance().getLineToWatchAux(s1, s2, absLineNum);
+		assertTrue(Integer.toString(returnedLine), returnedLine==2);
 	}
 }
